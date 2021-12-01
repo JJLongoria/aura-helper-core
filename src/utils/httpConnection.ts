a4897d92db1792dfa445a688408adedd2c92fa40
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-01 23:18:52 +0100

    Transform utils/httpsConnection.js file to TS

---
+++ src/utils/httpConnection.ts
@@ -0,0 +1,30 @@
+const https = require('https');
+
+/**
+ * Class to make HTTP request
+ */
+export class HTTPConnection {
+
+    /**
+     * Method to make a GET HTTP Request
+     * @param {string} endpoint URL to make the request
+     *  
+     * @returns {Promise<string>} Returns the URL content
+     */
+    static getRequest(endpoint: string) {
+        return new Promise<string>(function (resolve, reject) {
+            https.get(endpoint, (resp: any) => {
+                let data = '';
+                resp.on('data', (chunk: any) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    resolve(data);
+                });
+            }).on("error", (err: Error) => {
+                reject(err);
+            });
+        });
+    }  
+
+}
