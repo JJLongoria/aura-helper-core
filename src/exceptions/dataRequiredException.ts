4bb1affa05596d98af059444fa21be8db7d0d9ad
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-01 23:37:29 +0100

    Transform exceptions/DataRequiredException.js file to TS

--- src/exceptions/dataRequiredException.ts
+++ src/exceptions/dataRequiredException.ts
@@ -1,15 +1,14 @@
 /**
  * Exception class to handle and throw errors when Required Data is not provided
  */
-class DataRequiredException extends Error {
+export class DataRequiredException extends Error {
 
     /**
      * Constructor to create the exception
-     * @param {String} dataName data or field name
+     * @param {string} dataName data or field name
      */
-    constructor(dataName) {
+    constructor(dataName: string) {
         super(dataName + ' is Required.');
         this.name = "DataRequiredException";
     }
 }
-module.exports = DataRequiredException;
