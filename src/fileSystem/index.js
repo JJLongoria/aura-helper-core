c353bcd52085c1d2b69af8e13775ea56e0286ffc
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-02 00:40:32 +0100

    Transform fileSystem/index.js file to TS

--- src/fileSystem/index.js
+++
@@ -1,4 +0,0 @@
-exports.FileReader = require('./fileReader');
-exports.FileWriter = require('./fileWriter');
-exports.FileChecker = require('./fileChecker');
-exports.PathUtils = require('./pathUtils');
