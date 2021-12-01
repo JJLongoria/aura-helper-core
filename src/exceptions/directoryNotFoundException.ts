0309abeffdf6fade1147f29035eac5c7eb1f9f89
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-01 23:38:08 +0100

    Transform exceptions/DirectoryNotFoundException.js file to TS

--- src/exceptions/directoryNotFoundException.ts
+++ src/exceptions/directoryNotFoundException.ts
@@ -1,16 +1,15 @@
 /**
  * Class to handle and throw errors when not found or cant access to a system directory
  */
-class DirectoryNotFoundException extends Error {
+export class DirectoryNotFoundException extends Error {
 
     /**
      * Constructor to create the exception
-     * @param {String} folderPath Not found folder path
-     * @param {String} folderName Optional folder name to show
+     * @param {string} folderPath Not found folder path
+     * @param {string} folderName Optional folder name to show
      */
-    constructor(folderPath, folderName) {
+    constructor(folderPath: string, folderName: string) {
         super(((folderName) ? (folderName + ' folder') : 'Folder') + ' ' + folderPath + ' does not exists or not have access to it');
         this.name = "DirectoryNotFoundException";
     }
 }
-module.exports = DirectoryNotFoundException;
