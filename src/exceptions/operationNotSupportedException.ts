e1562d5ed34af0a20b646330960f1992dc3b91eb
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-01 23:40:48 +0100

    Transform exceptions/operationNotSupportedException.js file to TS

--- src/exceptions/operationNotSupportedException.ts
+++ src/exceptions/operationNotSupportedException.ts
@@ -1,15 +1,14 @@
 /**
  * Class to handle and throw errors when execute not supported operations
  */
-class OperationNotSupportedException extends Error {
+export class OperationNotSupportedException extends Error {
 
 	/**
 	 * Constructor to create the exception
-	 * @param {String} message Exception message
+	 * @param {string} message Exception message
 	 */
-	constructor(message) {
+	constructor(message: string) {
 		super(message);
 		this.name = "OperationNotSupportedException";
 	}
 }
-module.exports = OperationNotSupportedException;
