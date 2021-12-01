4fd27092162f12369ad010bd8381d847cc25649d
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-01 23:28:21 +0100

    Transform utils/utils.js file to TS

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,157 +1,158 @@
 /**
  * Class with util methods to work with JS Objects, functions...
  */
-class Utils {
+export class Utils {
 
     /**
      * Method to force to put the data into an array if the data must be an array
-     * @param {Object} data Data to force be an array
+     * @param {any} data Data to force be an array
      * 
-     * @returns {Array<Object>} Returns an array with the data or undefined if data is undefined
+     * @returns {Array<any>} Returns an array with the data or undefined if data is undefined
      */
-    static forceArray(data) {
-        if (data === undefined)
+    static forceArray(data: any): any[] {
+        if (data === undefined) {
             return data;
+        }
         return (Array.isArray(data)) ? data : [data];
     }
 
     /**
      * Method to clone an object
-     * @param {Object} obj Object to clone
+     * @param {any} obj Object to clone
      * 
-     * @returns {Object} Returns the cloned object
+     * @returns {any} Returns the cloned object
      */
-    static clone(obj) {
+    static clone(obj: any): any {
         return JSON.parse(JSON.stringify(obj));
     }
 
     /**
      * Method to check if the value is an object
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is an object, false in otherwise
+     * @returns {boolean} true if the value is an object, false in otherwise
      */
-    static isObject(value) {
+    static isObject(value: any): boolean {
         return !Utils.isArray(value) && typeof value === 'object';
     }
 
     /**
      * Method to check if the value is a string
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a string, false in otherwise
+     * @returns {boolean} true if the value is a string, false in otherwise
      */
-    static isString(value) {
+    static isString(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'string';
     }
 
     /**
      * Method to check if the value is a number
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a number, false in otherwise
+     * @returns {boolean} true if the value is a number, false in otherwise
      */
-    static isNumber(value) {
+    static isNumber(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'number';
     }
 
     /**
      * Method to check if the value is a BigInt
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a BigInt, false in otherwise
+     * @returns {boolean} true if the value is a BigInt, false in otherwise
      */
-    static isBigInt(value) {
+    static isBigInt(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'bigint';
     }
 
     /**
      * Method to check if the value is a symbol
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a symbol, false in otherwise
+     * @returns {boolean} true if the value is a symbol, false in otherwise
      */
-    static isSymbol(value) {
+    static isSymbol(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'symbol';
     }
 
     /**
      * Method to check if the value is a boolean
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a boolean, false in otherwise
+     * @returns {boolean} true if the value is a boolean, false in otherwise
      */
-    static isBoolean(value) {
+    static isBoolean(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'boolean';
     }
 
     /**
      * Method to check if the value is a function
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is a function, false in otherwise
+     * @returns {boolean} true if the value is a function, false in otherwise
      */
-    static isFunction(value) {
+    static isFunction(value: any): boolean {
         return !Utils.isNull(value) && typeof value === 'function';
     }
 
     /**
      * Method to check if the value is an array
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is an array, false in otherwise
+     * @returns {boolean} true if the value is an array, false in otherwise
      */
-    static isArray(value) {
+    static isArray(value: any): boolean {
         return !Utils.isNull(value) && Array.isArray(value);
     }
 
     /**
      * Method to check if the value is null or undefined
-     * @param {Object} value Value to check
+     * @param {any} value Value to check
      * 
-     * @returns {Boolean} true if the value is null or undefined, false in otherwise
+     * @returns {boolean} true if the value is null or undefined, false in otherwise
      */
-    static isNull(value) {
+    static isNull(value: any): boolean {
         return value === undefined || value === null;
     }
 
     /**
      * Method to check if an object has keys
-     * @param {Object} value Object to check
+     * @param {any} value Object to check
      * 
-     * @returns {Boolean} true if the object has keys, false in otherwise
+     * @returns {boolean} true if the object has keys, false in otherwise
      */
-    static hasKeys(value) {
+    static hasKeys(value: any): boolean {
         return !Utils.isNull(value) && Utils.isObject(value) && Object.keys(value).length > 0;
     }
 
     /**
      * Method to count the keys from an object
-     * @param {Object} value Object to get the keys
+     * @param {any} value Object to get the keys
      * 
-     * @returns {Number} Returns the keys from the object
+     * @returns {number} Returns the keys from the object
      */
-    static countKeys(value) {
+    static countKeys(value: any): number {
         return (Utils.hasKeys(value)) ? Object.keys(value).length : 0;
     }
 
     /**
      * Method to get the first element from an object
-     * @param {Object} value Object to get the first element
+     * @param {any} value Object to get the first element
      * 
-     * @returns {Object} Returns the first element data
+     * @returns {any} Returns the first element data
      */
-    static getFirstElement(value) {
+    static getFirstElement(value: any): any {
         return (Utils.hasKeys(value)) ? value[Object.keys(value)[0]] : 0;
     }
 
     /**
      * Method to get the last element from an object
-     * @param {Object} value Object to get the last element
+     * @param {any} value Object to get the last element
      * 
-     * @returns {Object} Returns the last element data
+     * @returns {any} Returns the last element data
      */
-    static getLastElement(value) {
+    static getLastElement(value: any): any {
         return (Utils.hasKeys(value)) ? value[Object.keys(value)[Object.keys(value).length - 1]] : 0;
     }
 
@@ -159,27 +160,29 @@ class Utils {
      * Method to get the callback function from function arguments
      * @param {arguments} args function arguments to get the callback
      * 
-     * @returns {Function} Returns a function if exists, or undefined if not exists. 
+     * @returns {Function | undefined} Returns a function if exists, or undefined if not exists. 
      */
-    static getCallbackFunction(args) {
-        if (Utils.isNull(args) || args.length == 0)
+    static getCallbackFunction(args: any[]): Function | undefined {
+        if (Utils.isNull(args) || args.length === 0) {
             return undefined;
+        }
         for (let i = 0; i < args.length; i++) {
-            if (Utils.isFunction(args[i]))
+            if (Utils.isFunction(args[i])) {
                 return args[i];
+            }
         }
         return undefined;
     }
 
     /**
      * Method to sort an Array. You can use fields from elements to sort and sort with case sensitive or insensitive
-     * @param {Array<*>} elements Array with the elements to sort
-     * @param {Array<String>} [fields] fields from child to sort
-     * @param {Boolean} [caseSensitive] true if want sort data with case sensitive
+     * @param {Array<any>} elements Array with the elements to sort
+     * @param {Array<string>} [fields] fields from child to sort
+     * @param {boolean} [caseSensitive] true if want sort data with case sensitive
      * 
-     * @returns {Array<Object>} Returns the array sorted
+     * @returns {Array<any>} Returns the array sorted
      */
-    static sort(elements, fields, caseSensitive) {
+    static sort(elements: any[], fields?: string[], caseSensitive?: boolean): any[] {
         if (Array.isArray(elements) && elements.length > 0) {
             elements.sort(function (a, b) {
                 if (fields && fields.length > 0) {
@@ -189,7 +192,7 @@ class Utils {
                     for (const field of fields) {
                         let valA = (a[field] !== undefined) ? a[field] : "";
                         let valB = (b[field] !== undefined) ? b[field] : "";
-                        if (counter == 0) {
+                        if (counter === 0) {
                             nameA = valA;
                             nameB = valB;
                         } else {
@@ -199,9 +202,9 @@ class Utils {
                         counter++;
                     }
                     if (Utils.isNumber(nameA) && Utils.isNumber(nameB)) {
-                        if(nameA > nameB){
+                        if (nameA > nameB) {
                             return 1;
-                        } else if(nameA < nameB){
+                        } else if (nameA < nameB) {
                             return -1;
                         } else {
                             return 0;
@@ -227,4 +230,3 @@ class Utils {
         return elements;
     }
 }
-module.exports = Utils;
