8148411e4ef41e598b32af38911ede6c7f9f86f1
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-01 23:43:11 +0100

    Transform exceptions/wrongFormatException.js file to TS

--- src/exceptions/wrongFormatException.ts
+++ src/exceptions/wrongFormatException.ts
@@ -1,15 +1,14 @@
 /**
  * Class to handle and throw errors when data format are wrong
  */
-class WrongFormatException extends Error {
+export class WrongFormatException extends Error {
 
 	/**
 	 * Constructor to create the exception
-	 * @param {String} message Exception message
+	 * @param {string} message Exception message
 	 */
-	constructor(message) {
+	constructor(message: string) {
 		super(message);
 		this.name = "WrongFormatException";
 	}
 }
-module.exports = WrongFormatException;
