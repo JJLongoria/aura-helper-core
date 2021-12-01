b296f0f778fa1d7c026ad058997d77c0c3f5c0d1
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-01 23:35:55 +0100

    Transform exceptions/cliManagerException.js file to TS

--- src/exceptions/cliManagerException.ts
+++ src/exceptions/cliManagerException.ts
@@ -1,15 +1,14 @@
 /**
  * Exception class to handle and throw errors when has errors with CLI Manager module
  */
- class CLIManagerException extends Error {
+ export class CLIManagerException extends Error {
 
     /**
      * Constructor to create the exception
-     * @param {String} message Exception message
+     * @param {string} message Exception message
      */
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "CLIManagerException";
     }
 }
-module.exports = CLIManagerException;
