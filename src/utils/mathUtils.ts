d348acffb5a34854c350e579a6d7e5618f211e7b
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-01 23:20:44 +0100

    Transform utils/mathUtils.js file to TS

---
+++ src/utils/mathUtils.ts
@@ -0,0 +1,20 @@
+/**
+ * Class to create Utils Mathematics methods
+ */
+export class MathUils {
+
+    /**
+     * Method to round a number with a selected decimal precission. By default, round with two decimals
+     * @param {number} number Number to round
+     * @param {number} [decimalNumbers] Decimal numbers precission
+     * 
+     * @returns {number} Returns a rounded number with the specified decimals
+     */
+    static round(number: number, decimalNumbers: number): number {
+        if (!decimalNumbers) {
+            decimalNumbers = 2;
+        }
+        return Number(Math.round(Number(number + 'e' + decimalNumbers)) + 'e-' + decimalNumbers);
+    }
+
+}
