69d89e7fd258983e2f0be24ae281159d2afe3461
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-01 23:42:10 +0100

    Transform exceptions/wrongDirectotyPathException.js file to TS

--- src/exceptions/wrongDirectoryPathException.ts
+++ src/exceptions/wrongDirectoryPathException.ts
@@ -1,16 +1,15 @@
 /**
  * Class to handle and throw errors when folder paths are wrong
  */
- class WrongDirectoryPathException extends Error {
+ export class WrongDirectoryPathException extends Error {
 
     /**
      * Constructor to create the exception
-     * @param {String} folderPath Wrong folder path
-     * @param {String} folderName Optional folder name to show
+     * @param {string} folderPath Wrong folder path
+     * @param {string} folderName Optional folder name to show
      */
-    constructor(folderPath, folderName) {
+    constructor(folderPath: string, folderName: string) {
         super('Wrong ' + ((folderName) ? (folderName) : 'folder') + ' path. Expect a folder path and receive ' + folderPath);
         this.name = "WrongDirectoryPathException";
     }
 }
-module.exports = WrongDirectoryPathException;
