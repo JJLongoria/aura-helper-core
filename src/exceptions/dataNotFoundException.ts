7d63c01488cbacec958ac344ded882cef1425abd
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-01 23:36:59 +0100

    Transform exceptions/dataNotFoundException.js file to TS

--- src/exceptions/dataNotFoundException.ts
+++ src/exceptions/dataNotFoundException.ts
@@ -1,15 +1,14 @@
 /**
  * Exception class to handle and throw errors when not data provided or not data found to execute operations
  */
- class DataNotFoundException extends Error {
+ export class DataNotFoundException extends Error {
 
     /**
      * Constructor to create the exception
-     * @param {String} message Exception message
+     * @param {string} message Exception message
      */
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "DataNotFoundException";
     }
 }
-module.exports = DataNotFoundException;
