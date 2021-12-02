1d1432a7fe58f4237293e12086bf4fea0a092715
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-02 23:11:51 +0100

    Transform types/bulkStatus.js file to TS

--- src/types/commit.ts
+++ src/types/commit.ts
@@ -15,11 +15,11 @@ export class Commit {
     /**
      * Create new Commit instance
      * @param {string | Commit} pointerOrObject Commit Pointer or Commit instance
-     * @param {string} author Commit author name
-     * @param {string} authorEmail Commit author email
-     * @param {CommitDate} date Commit Date
-     * @param {string} title Commit title
-     * @param {string} message Commit message
+     * @param {string} [author] Commit author name
+     * @param {string} [authorEmail] Commit author email
+     * @param {CommitDate} [date] Commit Date
+     * @param {string} [title] Commit title
+     * @param {string} [message] Commit message
      */
     constructor(pointerOrObject: string | Commit, author?: string, authorEmail?: string, date?: CommitDate, title?: string, message?: string) {
         if (pointerOrObject instanceof Commit) {
