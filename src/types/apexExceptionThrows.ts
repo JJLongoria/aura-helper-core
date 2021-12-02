3a36cbcda376a5b1b1957f85fa5aa2431253a099
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-02 21:16:34 +0100

    Transform types/apexExceptionThrows.js file to TS

---
+++ src/types/apexExceptionThrows.ts
@@ -0,0 +1,19 @@
+import { ApexNodeTypes } from "../values";
+import { ApexNode } from "./apexNode";
+import { Token } from "./token";
+
+/**
+ * Class to represent an Apex Interface Node when Parsing Apex Code
+ */
+export class ApexExceptionThrows extends ApexNode {
+
+    /**
+     * Constructor to create an ApexExceptionThrows instance
+     * @param {string | ApexExceptionThrows} idOrException Node id or Object with ApexInterface fields
+     * @param {string} [name] Node name
+     * @param {Token} [startToken] Node start token
+     */
+    constructor(idOrException: string | ApexExceptionThrows, name?: string, startToken?: Token) {
+        super(idOrException, ApexNodeTypes.THROWS, name, startToken);
+    }
+}
