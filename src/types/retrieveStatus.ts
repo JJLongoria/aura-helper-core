bf00de3a33c520c148bb3065596a0d6cee0c18a8
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-02 21:57:23 +0100

    Transform types/RetrieveStatus.js file to TS

---
+++ src/types/retrieveStatus.ts
@@ -0,0 +1,36 @@
+/**
+ * Class to represent a Retrieve Status JSON Response
+ */
+export class RetrieveStatus {
+
+    id: string;
+    status?: string;
+    done: boolean;
+    success: boolean;
+    zipFilePath?: string;
+
+    /**
+     * Create new Retrieve Status instance
+     * @param {string | RetrieveStatus} idOrStatus Retrieve Id or Retrieve Status instance
+     * @param {string} status Retrieve Status
+     * @param {boolean} done True if retrieve is done
+     * @param {boolean} success True if retrieve is success
+     * @param {string} zipFilePath Path to the retrieved zip file
+     */
+    constructor(idOrStatus: string | RetrieveStatus, status?: string, done?: boolean, success?: boolean, zipFilePath?: string) {
+        if (idOrStatus instanceof RetrieveStatus) {
+            this.id = idOrStatus.id;
+            this.status = idOrStatus.status;
+            this.done = idOrStatus.done;
+            this.success = idOrStatus.success;
+            this.zipFilePath = idOrStatus.zipFilePath;
+        } else {
+            this.id = idOrStatus;
+            this.status = status;
+            this.done = done || false;
+            this.success = success || false;
+            this.zipFilePath = zipFilePath;
+        }
+    }
+
+}
