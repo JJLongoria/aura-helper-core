401f6fd558a099acc677ffe709d6c29a40553b63
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-02 22:37:09 +0100

    Transform types/PackageGeneratorResult.js file to TS

---
+++ src/types/packageGeneratorResult.ts
@@ -0,0 +1,27 @@
+/**
+ * Class to represent an Aura Helper Package Generator Result
+ */
+export class PackageGeneratorResult {
+
+    package?: string;
+    destructiveChanges?: string;
+    destructiveChangesPost?: string;
+
+    /**
+     * Create new Package Generator Result instance
+     * @param packageFileOrPackageResult Package generated file path or Package Generator Result instance
+     * @param destructiveChanges Destructive changes generated file path
+     * @param destructiveChangesPost Destructive changes post generated file path
+     */
+    constructor(packageFileOrPackageResult: string | PackageGeneratorResult, destructiveChanges?: string, destructiveChangesPost?: string) {
+        if(packageFileOrPackageResult instanceof PackageGeneratorResult){
+            this.package = packageFileOrPackageResult.package;
+            this.destructiveChanges = packageFileOrPackageResult.destructiveChanges;
+            this.destructiveChangesPost = packageFileOrPackageResult.destructiveChangesPost;
+        } else {
+            this.package = packageFileOrPackageResult;
+            this.destructiveChanges = destructiveChanges;
+            this.destructiveChangesPost = destructiveChangesPost;
+        }
+    }
+}
