6c53d2d326c4b531a7ff1e19bea0575a05c411e4
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-02 17:11:13 +0100

    Transform utils/index.ts file to TS

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-exports.MathUtils = require('./mathUtils');
-exports.HTTPConnection = require('./httpConnection');
-exports.OSUtils = require('./osUtils');
-exports.StrUtils = require('./strUtils');
-exports.Utils = require('./utils');
-exports.Validator = require('./validator');
-exports.ProjectUtils = require('./projectUtils');
-exports.MetadataUtils = require('./metadataUtils');
-
