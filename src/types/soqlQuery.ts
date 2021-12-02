d34e9429e73cc783a577b2a2999000d9d1ae439a
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-02 21:42:39 +0100

    Transform types/SOQLField.js file to TS

--- src/types/soqlQuery.ts
+++ src/types/soqlQuery.ts
@@ -1,5 +1,6 @@
 import { ApexNodeTypes } from "../values";
 import { ApexNode } from "./apexNode";
+import { SOQLField } from "./soqlField";
 import { Token } from "./token";
 
 /**
@@ -11,15 +12,15 @@ export class SOQLQuery extends ApexNode {
     where?: any;
     orderBy?: any;
     groupBy?: any;
-    projection?: string[];
+    projection?: SOQLField[];
     isDinamyc?: boolean;
     limit?: number;
 
     /**
      * Method to create new SOQL Query
      * @param {string | SOQLQuery} idOrQuery Node Id or query instance
-     * @param {string} name Node name
-     * @param {Token} startToken Node start token
+     * @param {string} [name] Node name
+     * @param {Token} [startToken] Node start token
      */
     constructor(idOrQuery: string | SOQLQuery, name?: string, startToken?: Token) {
         super(idOrQuery, ApexNodeTypes.SOQL, name, startToken);
