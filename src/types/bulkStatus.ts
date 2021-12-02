e7ff7f10f392c5c255f0811cd73188057a18cd7b
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-02 23:11:51 +0100

    Transform types/bulkStatus.js file to TS

---
+++ src/types/bulkStatus.ts
@@ -0,0 +1,39 @@
+/**
+ * Class to represent a Salesforce Bulk operation status 
+ */
+export class BulkStatus {
+
+    id: string;
+    jobId?: string;
+    state?: string;
+    createdDate?: string;
+    numberRecordsProcessed?: number;
+    numberRecordsFailed?: number;
+    totalProcessingTime?: number;
+    apiActiveProcessingTime?: number;
+    apexProcessingTime?: number;
+
+    /**
+     * Create new Bulk Status instance
+     * @param {string | BulkStatus} idOrBulkStatus Bulk Job Id or Bulk Status instance
+     * @param {string} [jobId] Job Id
+     * @param {string} [state] Bulk state
+     */
+    constructor(idOrBulkStatus: string | BulkStatus, jobId?: string, state?: string){
+        if(idOrBulkStatus instanceof BulkStatus){
+            this.id = idOrBulkStatus.id;
+            this.jobId = idOrBulkStatus.jobId;
+            this.state = idOrBulkStatus.state;
+            this.createdDate = idOrBulkStatus.createdDate;
+            this.numberRecordsProcessed = idOrBulkStatus.numberRecordsProcessed;
+            this.numberRecordsFailed = idOrBulkStatus.numberRecordsFailed;
+            this.totalProcessingTime = idOrBulkStatus.totalProcessingTime;
+            this.apiActiveProcessingTime = idOrBulkStatus.apiActiveProcessingTime;
+            this.apexProcessingTime = idOrBulkStatus.apexProcessingTime;
+        } else {
+            this.id = idOrBulkStatus;
+            this.jobId = jobId;
+            this.state = state;
+        }
+    }
+}
