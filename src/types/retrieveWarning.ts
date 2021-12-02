40d43ad00e155eece714464cc21a3732d70487e6
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-02 22:10:59 +0100

    Transform types/RetrieveResult.js file to TS

--- src/types/retrieveWarning.ts
+++ src/types/retrieveWarning.ts
@@ -1,5 +1,5 @@
 /**
- * Class to represent a Retrieve Warning JSON Response
+ * Class to represent a Retrieve Warning section on Retriieve Result JSON Response
  */
 export class RetrieveWarning {
 
@@ -9,7 +9,7 @@ export class RetrieveWarning {
     /**
      * Create new Retrieve Warning instance
      * @param {string | RetrieveWarning} fileNameOrWarning Retrieve file name or Warning instance
-     * @param {string} problem Problem description
+     * @param {string} [problem] Problem description
      */
     constructor(fileNameOrWarning: string | RetrieveWarning, problem?: string) {
         if (fileNameOrWarning instanceof RetrieveWarning) {
