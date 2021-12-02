9a0ec928ba36ab7d85942da63fd979202bbbfe4d
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-02 23:17:03 +0100

    Transform types/authOrg.js file to TS

---
+++ src/types/authOrg.ts
@@ -0,0 +1,39 @@
+/**
+ * Class to represent a SFDX Auth Org
+ */
+export class AuthOrg {
+
+    alias: string;
+    username?: string;
+    orgId?: string;
+    instanceUrl?: string;
+    accessToken?: string;
+    oauthMethod?: string;
+
+    /**
+     * Create new Auth Org instance
+     * @param {string | AuthOrg} aliasOrAuthOrg Org Alias or Auth Org instance
+     * @param {string} [username] Org username
+     * @param {string} [orgId] Org Id
+     * @param {string} [instanceUrl] Org Instance URl
+     * @param {string} [accessToken] Org Access Token
+     * @param {string} [oauthMethod] Org Oauth method
+     */
+    constructor(aliasOrAuthOrg: string | AuthOrg, username?: string, orgId?: string, instanceUrl?: string, accessToken?: string, oauthMethod?: string) {
+        if (aliasOrAuthOrg instanceof AuthOrg) {
+            this.alias = aliasOrAuthOrg.alias;
+            this.username = aliasOrAuthOrg.username;
+            this.orgId = aliasOrAuthOrg.orgId;
+            this.instanceUrl = aliasOrAuthOrg.instanceUrl;
+            this.accessToken = aliasOrAuthOrg.accessToken;
+            this.oauthMethod = aliasOrAuthOrg.oauthMethod
+        } else {
+            this.alias = aliasOrAuthOrg;
+            this.username = username;
+            this.orgId = orgId;
+            this.instanceUrl = instanceUrl;
+            this.accessToken = accessToken;
+            this.oauthMethod = oauthMethod;
+        }
+    }
+}
