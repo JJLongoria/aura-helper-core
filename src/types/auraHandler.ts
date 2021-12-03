acf4cb80d9922ff5f0098719990e4528b45eb582
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-03 14:09:04 +0100

    Transform types/auraHandler.js file to TS

---
+++ src/types/auraHandler.ts
@@ -0,0 +1,38 @@
+import { AuraNodeTypes } from "../values";
+import { AuraNode } from "./auraNode";
+import { AuraTagData } from "./auraTagData";
+import { Token } from "./token";
+
+/**
+ * Class to represent an Aura Handler Node on XML language when analize and parse Aura XML Files
+ */
+export class AuraHandler extends AuraNode {
+
+    action?: AuraTagData;
+    event?: AuraTagData;
+    phase?: AuraTagData;
+    includeFacets?: AuraTagData;
+
+    /**
+     * Create new Aura Handler instance
+     * @param {string | AuraHandler} quelifiedNameOrNode Qualified XML tag or Aura Node instance
+     * @param {Token} [token] Tag first token
+     */
+    constructor(quelifiedNameOrNode: string | AuraHandler, token?: Token) {
+        super(quelifiedNameOrNode, AuraNodeTypes.HANDLER, token);
+        if (quelifiedNameOrNode instanceof AuraHandler) {
+            this.action = quelifiedNameOrNode.action;
+            this.event = quelifiedNameOrNode.event;
+            this.phase = quelifiedNameOrNode.phase;
+            this.includeFacets = quelifiedNameOrNode.includeFacets;
+            this.name = quelifiedNameOrNode.name;
+        } else {
+            this.action = undefined;
+            this.event = undefined;
+            this.phase = undefined;
+            this.includeFacets = undefined;
+            this.name = undefined;
+        }
+    }
+
+}
