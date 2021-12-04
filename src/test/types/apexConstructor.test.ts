846af5b9f01c0dc021da371298334798fda47193
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 20:03:41 +0100

    Transformed test/types/apexConstructor.test.js into ts files

--- src/test/types/apexConstructor.test.ts
+++ src/test/types/apexConstructor.test.ts
@@ -1,16 +1,15 @@
-const ApexConstructor = require('../../../src/types/apexConstructor');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import { ApexConstructor, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexConstructor.js', () => {
     test('Testing instance', () => {
         const apexConstructor = new ApexConstructor('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexConstructor.id).toMatch('id');
         expect(apexConstructor.name).toMatch('name');
-        expect(apexConstructor.nodeType).toMatch(ApexNodeType.CONSTRUCTOR);
+        expect(apexConstructor.nodeType).toMatch(ApexNodeTypes.CONSTRUCTOR);
         const apexConstructor2 = new ApexConstructor(apexConstructor);
         expect(apexConstructor2.id).toMatch('id');
         expect(apexConstructor2.name).toMatch('name');
-        expect(apexConstructor2.nodeType).toMatch(ApexNodeType.CONSTRUCTOR);
+        expect(apexConstructor2.nodeType).toMatch(ApexNodeTypes.CONSTRUCTOR);
     });
 });
