7bda84b5f07c7c87c6deb6e234dc4cbdd746dda4
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:37:18 +0100

    Transformed test/types/auraAttribute.test.js into ts files

--- src/test/types/auraAttribute.test.ts
+++ src/test/types/auraAttribute.test.ts
@@ -1,14 +1,13 @@
-const AuraAttribute = require('../../../src/types/auraAttribute');
-const Token = require('../../../src/types/token');
-const AuraNodeType = require('../../../src/values/auraNodeType');
+import { AuraAttribute, Token } from "../../types";
+import { AuraNodeTypes } from "../../values";
 
 describe('Testing ./src/types/auraAttribute.js', () => {
     test('Testing instance', () => {
         const auraAttribute = new AuraAttribute('c:name', new Token('type', 'text', 1, 0, false));
         expect(auraAttribute.name).toMatch('name');
-        expect(auraAttribute.nodeType).toMatch(AuraNodeType.ATTRIBUTE);
+        expect(auraAttribute.nodeType).toMatch(AuraNodeTypes.ATTRIBUTE);
         const auraAttribute2 = new AuraAttribute(auraAttribute);
         expect(auraAttribute2.name).toMatch('name');
-        expect(auraAttribute2.nodeType).toMatch(AuraNodeType.ATTRIBUTE);
+        expect(auraAttribute2.nodeType).toMatch(AuraNodeTypes.ATTRIBUTE);
     });
 });
