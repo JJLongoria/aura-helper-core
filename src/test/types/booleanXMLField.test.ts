75f740ee39ee4ef04483f79df1b9558b46d83e0f
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 21:46:56 +0100

    Transformed test/types/booleanXMLField.test.js into ts files

--- src/test/types/booleanXMLField.test.ts
+++ src/test/types/booleanXMLField.test.ts
@@ -1,5 +1,5 @@
-const BooleanXMLField = require('../../../src/types/booleanXMLField');
-const DataTypes = require('../../../src/values/datatypes');
+import { BooleanXMLField } from "../../types";
+import { Datatypes } from "../../values";
 
 describe('Testing ./src/types/booleanXMLField.js', () => {
     test('Testing instance', () => {
@@ -9,10 +9,10 @@ describe('Testing ./src/types/booleanXMLField.js', () => {
         expect(field.transformValue(false)).toEqual(false);
         expect(field.key).toMatch('key');
         expect(field.label).toMatch('Label');
-        expect(field.datatype).toMatch(DataTypes.BOOLEAN);
+        expect(field.datatype).toMatch(Datatypes.BOOLEAN);
         const field2 = new BooleanXMLField(field);
         expect(field2.key).toMatch('key');
         expect(field2.label).toMatch('Label');
-        expect(field2.datatype).toMatch(DataTypes.BOOLEAN);
+        expect(field2.datatype).toMatch(Datatypes.BOOLEAN);
     });
 });
