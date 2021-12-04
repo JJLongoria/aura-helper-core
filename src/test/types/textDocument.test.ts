b9cbb10a9f019f980b28f204a4a2b3b0135dd1b2
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 23:35:23 +0100

    Transformed test/types/textDocument.test.js into ts files

--- src/test/types/textDocument.test.ts
+++ src/test/types/textDocument.test.ts
@@ -1,16 +1,16 @@
-const TextDocument = require('../../../src/types/textDocument');
+import { TextDocument, TextLine } from "../../types";
 
 describe('Testing ./src/types/textDocument.js', () => {
     test('Testing instance', () => {
-        const textDocument = new TextDocument('fileName', 'uri', ['lines'], 'apex');
+        const textDocument = new TextDocument('fileName', 'uri', [new TextLine('lines', 1)], 'apex');
         expect(textDocument.fileName).toEqual('fileName');
         expect(textDocument.uri).toEqual('uri');
-        expect(textDocument.lines).toEqual(['lines']);
+        expect(textDocument.lines).toEqual([new TextLine('lines', 1)]);
         expect(textDocument.lineCount).toEqual(1);
         const textDocument2 = new TextDocument(textDocument);
         expect(textDocument2.fileName).toEqual('fileName');
         expect(textDocument2.uri).toEqual('uri');
-        expect(textDocument2.lines).toEqual(['lines']);
+        expect(textDocument2.lines).toEqual([new TextLine('lines', 1)]);
         expect(textDocument2.lineCount).toEqual(1);
     });
 });
