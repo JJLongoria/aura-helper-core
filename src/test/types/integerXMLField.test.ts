b1ef34a393d9d12f1fac1a4bbec22245fafa45a2
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:50:26 +0100

    Transformed test/types/intergerXMLField.test.js into ts files

--- src/test/types/integerXMLField.test.ts
+++ src/test/types/integerXMLField.test.ts
@@ -1,5 +1,5 @@
-const IntegerXMLField = require('../../../src/types/integerXMLField');
-const DataTypes = require('../../../src/values/datatypes');
+import { IntegerXMLField } from "../../types";
+import { Datatypes } from "../../values";
 
 describe('Testing ./src/types/integerXMLField.js', () => {
     test('Testing instance', () => {
@@ -8,10 +8,10 @@ describe('Testing ./src/types/integerXMLField.js', () => {
         expect(field.transformValue(5)).toEqual(5);
         expect(field.key).toMatch('key');
         expect(field.label).toMatch('Label');
-        expect(field.datatype).toMatch(DataTypes.INTEGER);
+        expect(field.datatype).toMatch(Datatypes.INTEGER);
         const field2 = new IntegerXMLField(field);
         expect(field2.key).toMatch('key');
         expect(field2.label).toMatch('Label');
-        expect(field2.datatype).toMatch(DataTypes.INTEGER);
+        expect(field2.datatype).toMatch(Datatypes.INTEGER);
     });
 });
