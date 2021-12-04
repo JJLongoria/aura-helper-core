3bf5f4d6a2a0343e8d0f5366f5c92399da9c189f
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 21:29:15 +0100

    Transformed test/types/apexMethod.test.js into ts files

--- src/test/types/apexMethod.test.ts
+++ src/test/types/apexMethod.test.ts
@@ -1,35 +1,19 @@
-const ApexMethod = require('../../../src/types/apexMethod');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import { ApexMethod, ApexVariable, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexMethod.js', () => {
     test('Testing instance', () => {
         const apexMethod = new ApexMethod('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexMethod.id).toMatch('id');
         expect(apexMethod.name).toMatch('name');
-        expect(apexMethod.nodeType).toMatch(ApexNodeType.METHOD);
+        expect(apexMethod.nodeType).toMatch(ApexNodeTypes.METHOD);
         expect(apexMethod.getFirstChild()).toEqual(apexMethod.getAbosluteFirstChild());
         expect(apexMethod.getLastChild()).toEqual(apexMethod.getAbsoluteLastChild());
-        apexMethod.addChild({
-            nodeType: ApexNodeType.VARIABLE,
-            name: 'apexVar2'
-        });
-        apexMethod.addChild({
-            nodeType: ApexNodeType.VARIABLE,
-            name: 'apexVar1'
-        });
-        apexMethod.addChild({
-            nodeType: ApexNodeType.VARIABLE,
-            name: 'apexVar3'
-        });
-        apexMethod.addParam({
-            nodeType: ApexNodeType.VARIABLE,
-            name: 'apexVar4'
-        });
-        apexMethod.addParam({
-            nodeType: ApexNodeType.VARIABLE,
-            name: 'apexVar5'
-        });
+        apexMethod.addChild(new ApexVariable('id', 'apexVar2'));
+        apexMethod.addChild(new ApexVariable('id', 'apexVar1'));
+        apexMethod.addChild(new ApexVariable('id', 'apexVar3'));
+        apexMethod.addParam(new ApexVariable('id', 'apexVar4'));
+        apexMethod.addParam(new ApexVariable('id', 'apexVar5'));
         apexMethod.getOrderedChilds();
         apexMethod.getOrderedParams();
         apexMethod.getOrderedVariables();
@@ -38,6 +22,6 @@ describe('Testing ./src/types/apexMethod.js', () => {
         const apexMethod2 = new ApexMethod(apexMethod);
         expect(apexMethod2.id).toMatch('id');
         expect(apexMethod2.name).toMatch('name');
-        expect(apexMethod2.nodeType).toMatch(ApexNodeType.METHOD);
+        expect(apexMethod2.nodeType).toMatch(ApexNodeTypes.METHOD);
     });
 });
