6ec77bdf20bb3573af2b8231fc34ea6e72f91569
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 21:43:50 +0100

    Transformed test/types/auraJSComment.test.js into ts files

--- src/test/types/auraJSComment.test.ts
+++ src/test/types/auraJSComment.test.ts
@@ -1,19 +1,18 @@
-const AuraJSComment = require('../../../src/types/auraJSComment');
-const Token = require('../../../src/types/token');
-const AuraNodeType = require('../../../src/values/auraNodeType');
+import { AuraJSComment, Token } from "../../types";
+import { AuraNodeTypes } from "../../values";
 
 describe('Testing ./src/types/auraJSComment.js', () => {
     test('Testing instance', () => {
-        const auraJSComment = new AuraJSComment('c:name', new Token('type', 'text', 1, 0, false));
+        const auraJSComment = new AuraJSComment();
         auraJSComment.addToken(new Token('comment.line.double-slash', '//', 0, 0, false));
         auraJSComment.addToken(new Token('comment.content', 'Esto', 0, 3, false));
         auraJSComment.addToken(new Token('comment.content', 'es', 0, 8, false));
         auraJSComment.addToken(new Token('comment.content', 'un', 0, 11, false));
         auraJSComment.addToken(new Token('comment.content', 'comentario', 0, 14, false));
         auraJSComment.processComment();
-        expect(auraJSComment.nodeType).toMatch(AuraNodeType.JS_COMMENT);
+        expect(auraJSComment.nodeType).toMatch(AuraNodeTypes.JS_COMMENT);
         expect(auraJSComment.description).toMatch('Esto es un comentario');
         const auraJSComment2 = new AuraJSComment(auraJSComment);
-        expect(auraJSComment2.nodeType).toMatch(AuraNodeType.JS_COMMENT);
+        expect(auraJSComment2.nodeType).toMatch(AuraNodeTypes.JS_COMMENT);
     });
 });
