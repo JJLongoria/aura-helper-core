c4b12dbb01c2327ba41707ef6a569de790dc9537
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:34:05 +0100

    Transformed test/types/apexVariable.test.js into ts files

--- src/test/types/apexVariable.test.ts
+++ src/test/types/apexVariable.test.ts
@@ -1,16 +1,15 @@
-const ApexVariable = require('../../../src/types/apexVariable');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import {  ApexVariable, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexVariable.js', () => {
     test('Testing instance', () => {
         const apexVariable = new ApexVariable('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexVariable.id).toMatch('id');
         expect(apexVariable.name).toMatch('name');
-        expect(apexVariable.nodeType).toMatch(ApexNodeType.VARIABLE);
+        expect(apexVariable.nodeType).toMatch(ApexNodeTypes.VARIABLE);
         const apexVariable2 = new ApexVariable(apexVariable);
         expect(apexVariable2.id).toMatch('id');
         expect(apexVariable2.name).toMatch('name');
-        expect(apexVariable2.nodeType).toMatch(ApexNodeType.VARIABLE);
+        expect(apexVariable2.nodeType).toMatch(ApexNodeTypes.VARIABLE);
     });
 });
