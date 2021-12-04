372a275edd186809733ae2a3a65b378f729c7696
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 21:39:53 +0100

    Transformed test/types/auraHandler.test.js into ts files

--- src/test/types/auraHandler.test.ts
+++ src/test/types/auraHandler.test.ts
@@ -1,15 +1,14 @@
-const AuraHandler = require('../../../src/types/auraHandler');
-const Token = require('../../../src/types/token');
-const AuraNodeType = require('../../../src/values/auraNodeType');
+import { AuraHandler, Token } from "../../types";
+import { AuraNodeTypes } from "../../values";
 
 describe('Testing ./src/types/auraHandler.js', () => {
     test('Testing instance', () => {
         const auraHandler = new AuraHandler('c:name', new Token('type', 'text', 1, 0, false));
         auraHandler.name = 'name';
         expect(auraHandler.name).toMatch('name');
-        expect(auraHandler.nodeType).toMatch(AuraNodeType.HANDLER);
+        expect(auraHandler.nodeType).toMatch(AuraNodeTypes.HANDLER);
         const auraHandler2 = new AuraHandler(auraHandler);
         expect(auraHandler2.name).toMatch('name');
-        expect(auraHandler2.nodeType).toMatch(AuraNodeType.HANDLER);
+        expect(auraHandler2.nodeType).toMatch(AuraNodeTypes.HANDLER);
     });
 });
