eda437c52ab0b32d118a6419187103262429e40e
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 23:26:21 +0100

    Transformed test/types/process.test.js into ts files

--- src/test/types/process.test.ts
+++ src/test/types/process.test.ts
@@ -1,38 +1,35 @@
-const Process = require('../../../src/types/process');
-const ProcessEvent = require('../../../src/values/processEvent');
+import { Process } from "../../types";
+import { ProcessEvent } from "../../values";
 
 describe('Testing ./src/types/process.js', () => {
-    test('Testing run() OK', async (done) => {
+    test('Testing run() OK', async () => {
         const process = new Process('cmd', ['/c', 'dir'], {});
         process.setProcessName('dir');
         process.setCWD('./');
         process.setJSONResponse(false);
         process.setMaxBuffer(50000);
-        process.run((event, data) => {
-            if(event === ProcessEvent.END)
-                done();
+        process.run(() => {
+
         });
     });
-    test('Testing run() Error Out', async (done) => {
+    test('Testing run() Error Out', async () => {
         const process = new Process('cmd', ['/css', 'dir'], {});
         process.setProcessName('dir');
         process.setCWD('./');
         process.setJSONResponse(false);
         process.setMaxBuffer(50000);
-        process.run((event, data) => {
-            if(event === ProcessEvent.END)
-                done();
+        process.run(() => {
+     
         });
     });
-    test('Testing kill() ', async (done) => {
+    test('Testing kill() ', async () => {
         const process = new Process('cmd', ['/c', 'force:mdapi:describemetadata', '-u', 'MyOrg', '--apiVersion', '50'], {});
         process.setProcessName('dir');
         process.setCWD('./');
         process.setJSONResponse(false);
         process.setMaxBuffer(50000);
-        process.run((event, data) => {
-            if(event === ProcessEvent.END)
-                done();
+        process.run(() => {
+
         });
         process.kill();
     });
