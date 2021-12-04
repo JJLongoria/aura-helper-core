2bf65e7e25c3ebf21346d70f887eba670494059d
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:30:26 +0100

    Transformed test/types/apexProperty.test.js into ts files

---
+++ src/test/types/apexProperty.test.ts
@@ -0,0 +1,17 @@
+import { ApexGetter, ApexProperty, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
+
+describe('Testing ./src/types/apexProperty.js', () => {
+    test('Testing instance', () => {
+        const apexProperty = new ApexProperty('id', 'name', new Token('type', 'text', 1, 0, false));
+        expect(apexProperty.id).toMatch('id');
+        expect(apexProperty.name).toMatch('name');
+        expect(apexProperty.nodeType).toMatch(ApexNodeTypes.PROPERTY);
+        apexProperty.addChild(new ApexGetter('id', 'apexGetter'));
+        apexProperty.addChild(new ApexGetter('id', 'apexSetter'));
+        const apexProperty2 = new ApexProperty(apexProperty);
+        expect(apexProperty2.id).toMatch('id');
+        expect(apexProperty2.name).toMatch('name');
+        expect(apexProperty2.nodeType).toMatch(ApexNodeTypes.PROPERTY);
+    });
+});
