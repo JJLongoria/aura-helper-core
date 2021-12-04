445f2957b128485523de60c8b383ac76cb25f1e7
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:27:58 +0100

    Transformed test/types/apexInterface.test.js into ts files

--- src/test/types/apexInterface.test.ts
+++ src/test/types/apexInterface.test.ts
@@ -1,16 +1,15 @@
-const ApexInterface = require('../../../src/types/apexInterface');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import { ApexInterface, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexInterface.js', () => {
     test('Testing instance', () => {
         const apexInterface = new ApexInterface('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexInterface.id).toMatch('id');
         expect(apexInterface.name).toMatch('name');
-        expect(apexInterface.nodeType).toMatch(ApexNodeType.INTERFACE);
+        expect(apexInterface.nodeType).toMatch(ApexNodeTypes.INTERFACE);
         const apexInterface2 = new ApexInterface(apexInterface);
         expect(apexInterface2.id).toMatch('id');
         expect(apexInterface2.name).toMatch('name');
-        expect(apexInterface2.nodeType).toMatch(ApexNodeType.INTERFACE);
+        expect(apexInterface2.nodeType).toMatch(ApexNodeTypes.INTERFACE);
     });
 });
