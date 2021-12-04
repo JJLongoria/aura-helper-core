2bc9432ae0fc6dd1dd8ef3bf75134a377909c06a
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-04 21:45:29 +0100

    Transformed test/types/auraRegisterEvent.test.js into ts files

--- src/test/types/auraRegisterEvent.test.ts
+++ src/test/types/auraRegisterEvent.test.ts
@@ -1,15 +1,14 @@
-const AuraRegisterEvent = require('../../../src/types/auraRegisterEvent');
-const Token = require('../../../src/types/token');
-const AuraNodeType = require('../../../src/values/auraNodeType');
+import { AuraRegisterEvent, Token } from "../../types";
+import { AuraNodeTypes } from "../../values";
 
 describe('Testing ./src/types/auraRegisterEvent.js', () => {
     test('Testing instance', () => {
         const auraRegisterEvent = new AuraRegisterEvent('c:name', new Token('type', 'text', 1, 0, false));
         auraRegisterEvent.name = 'name';
         expect(auraRegisterEvent.name).toMatch('name');
-        expect(auraRegisterEvent.nodeType).toMatch(AuraNodeType.REGISTER_EVENT);
+        expect(auraRegisterEvent.nodeType).toMatch(AuraNodeTypes.REGISTER_EVENT);
         const auraRegisterEvent2 = new AuraRegisterEvent(auraRegisterEvent);
         expect(auraRegisterEvent2.name).toMatch('name');
-        expect(auraRegisterEvent2.nodeType).toMatch(AuraNodeType.REGISTER_EVENT);
+        expect(auraRegisterEvent2.nodeType).toMatch(AuraNodeTypes.REGISTER_EVENT);
     });
 });
