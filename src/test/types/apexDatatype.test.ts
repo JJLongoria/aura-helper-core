12c140b2d2240c30dd7130df48f7701373961201
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 20:06:11 +0100

    Transformed test/types/apexDatatype.test.js into ts files

--- src/test/types/apexDatatype.test.ts
+++ src/test/types/apexDatatype.test.ts
@@ -1,16 +1,15 @@
-const ApexDatatype = require('../../../src/types/apexDatatype');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import { ApexDatatype, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexDatatype.js', () => {
     test('Testing instance', () => {
         const apexDatatype = new ApexDatatype('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexDatatype.id).toMatch('id');
         expect(apexDatatype.name).toMatch('name');
-        expect(apexDatatype.nodeType).toMatch(ApexNodeType.DATATYPE);
+        expect(apexDatatype.nodeType).toMatch(ApexNodeTypes.DATATYPE);
         const apexDatatype2 = new ApexDatatype(apexDatatype);
         expect(apexDatatype2.id).toMatch('id');
         expect(apexDatatype2.name).toMatch('name');
-        expect(apexDatatype2.nodeType).toMatch(ApexNodeType.DATATYPE);
+        expect(apexDatatype2.nodeType).toMatch(ApexNodeTypes.DATATYPE);
     });
 });
