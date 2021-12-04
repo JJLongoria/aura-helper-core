d6610bc69f14b5f32347d021466b8ec28e9c8223
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:31:54 +0100

    Transformed test/types/apexStaticConstructor.test.js into ts files

--- src/test/types/apexStaticConstructor.test.ts
+++ src/test/types/apexStaticConstructor.test.ts
@@ -1,16 +1,15 @@
-const ApexStaticConstructor = require('../../../src/types/apexStaticConstructor');
-const Token = require('../../../src/types/token');
-const ApexNodeType = require('../../../src/values/apexNodeTypes');
+import { ApexStaticConstructor, Token } from "../../types";
+import { ApexNodeTypes } from "../../values";
 
 describe('Testing ./src/types/apexStaticConstructor.js', () => {
     test('Testing instance', () => {
         const apexStaticConstructor = new ApexStaticConstructor('id', 'name', new Token('type', 'text', 1, 0, false));
         expect(apexStaticConstructor.id).toMatch('id');
         expect(apexStaticConstructor.name).toMatch('name');
-        expect(apexStaticConstructor.nodeType).toMatch(ApexNodeType.STATIC_CONSTRUCTOR);
+        expect(apexStaticConstructor.nodeType).toMatch(ApexNodeTypes.STATIC_CONSTRUCTOR);
         const apexStaticConstructor2 = new ApexStaticConstructor(apexStaticConstructor);
         expect(apexStaticConstructor2.id).toMatch('id');
         expect(apexStaticConstructor2.name).toMatch('name');
-        expect(apexStaticConstructor2.nodeType).toMatch(ApexNodeType.STATIC_CONSTRUCTOR);
+        expect(apexStaticConstructor2.nodeType).toMatch(ApexNodeTypes.STATIC_CONSTRUCTOR);
     });
 });
