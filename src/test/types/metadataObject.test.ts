872d0836dbe8c70aad0eec06357acf621ba42f62
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-04 21:53:24 +0100

    Transformed test/types/metadataObject.test.js into ts files

--- src/test/types/metadataObject.test.ts
+++ src/test/types/metadataObject.test.ts
@@ -1,21 +1,15 @@
-const MetadataObject = require('../../../src/types/metadataObject');
+import { MetadataItem, MetadataObject } from "../../types";
 
 describe('Testing ./src/types/metadataObject.js', () => {
     test('Testing instance', () => {
         let obj1 = new MetadataObject('CustomField', false, 'objects');
         expect(obj1.name).toEqual('CustomField');
-        const object = {
-            name: 'Account',
-            checked: false,
-            path: 'Account',
-            childs: {},
-        };
-        let obj2 = new MetadataObject(object);
+        let obj2 = new MetadataObject(obj1);
         expect(obj2.name).toEqual('Account');
-        obj2.addChild('name', 'test');
+        obj2.addChild('name', new MetadataItem('test'));
         expect(obj2.getChild('name').name).toEqual('test');
         expect(obj2.childsCount()).toEqual(1);
-        expect(obj2.haveChilds()).toBeTruthy();
+        expect(obj2.hasChilds()).toBeTruthy();
         expect(obj2.allChildsChecked()).toBeFalsy();
     });
 });
